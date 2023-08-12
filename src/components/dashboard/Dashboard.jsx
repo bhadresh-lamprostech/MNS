c949ef7c55f039eb724d8c60747d176dac1b9b8d
Prajapati Yash <77118917+prajapati-yash@users.noreply.github.com>
2023-08-12 11:48:43 +0530

    Boiler Plate

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <>
+       <div className='border border-dark bg-white'>
+                <h1>Dash</h1>
+       </div>
+    
+    </>
+  )
+}
+
+export default Dashboard
